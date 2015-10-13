28b4cf80409b9ddca6b98f8c8556b2cdb78054e4
mborden <mborden@uccs.edu>
2015-10-13 16:10:52 -0600

    update
    It's plotting a line, but can't do the star because its a 1 x 4 times an
    11 x 2. Notation is wrong in the dmlread

--- part3.m
+++ part3.m
@@ -3,38 +3,19 @@ clc
 clear all
 clf
 
-%%Set the x and y linspace
-Ax = linspace(0,0,50);
-Ay = linspace(-2,2,50);
-
-%%Set the x and y limits
-xlim([-10 10])
-ylim([-10 10])
-
-%%Plot the x and y
-plot(Ax,Ay);
-%%Set the linspace
-xDiag = linspace(0,2,25);
-
-%%Set the diagnols
-A = xDiag;
-B = -xDiag;
-
-%%Plot the diagnols
-plot(xDiag,A);
-plot(xDiag,B);
 %%fileID reads in the text file
-fileID = fopen('part3.txt','r');
-%%M =csvread('part3.txt');
+%%fileID = fopen('part3.txt','r');
 %%This specifies the format of the file that we are reading in
 formatSpec = '%f %f';
 
 %%This sets the size of the matrice to be read in
-sizeA = [2 6];
-amat = A(1, 3:6);
-%%Reads the text file, specifies the format, and sets the size
-A = fscanf(fileID, formatSpec, sizeA);
+%%sizeA = [1 6];
 
+%%Reads the text file, specifies the format, and sets the size
+filename = 'newpart3.txt';
+M = dlmread(filename, ' ',1,2);
+
+%%amat = A(1, 3:6);
 %%Atrans = dlmread('part3.txt','\t',0,6);
 
 %%Plots A
@@ -45,13 +26,13 @@ fileID = fopen('shape2.txt','r');
 
 formatSpec = '%f %f';
 
-sizeB = [2 12];
+sizeB = [11 2];
 
 B = fscanf(fileID, formatSpec, sizeB);
 
 hold on
 
 plot(B(1,:), B(2 ,:),'r');
-
-C = A' * B;
-plot(C(1,:), C(2 ,:),'r');
+hold on 
+C =  M' * B';
+plot(C(1,:), C(2 ,:),'g');
