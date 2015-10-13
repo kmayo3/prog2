50fdc0e0220720a0bce0687842126117a9fab093
mborden <mborden@uccs.edu>
2015-10-12 21:30:37 -0600

    translations
    translation

--- part3.m
+++ part3.m
@@ -3,6 +3,26 @@ clc
 clear all
 clf
 
+%%Set the x and y linspace
+Ax = linspace(-10,10,50);
+Ay = linspace(-10,10,50);
+
+%%Set the x and y limits
+xlim([-10 10])
+ylim([-10 10])
+
+%%Plot the x and y
+plot(Ax,Ay);
+%%Set the linspace
+xDiag = linspace(0,10,50);
+
+%%Set the diagnols
+A = xDiag;
+B = -xDiag;
+
+%%Plot the diagnols
+plot(xDiag,A);
+plot(xDiag,B);
 %%fileID reads in the text file
 fileID = fopen('part3.txt','r');
 
@@ -10,7 +30,7 @@ fileID = fopen('part3.txt','r');
 formatSpec = '%f %f';
 
 %%This sets the size of the matrice to be read in
-sizeA = [3 2];
+sizeA = [2 3];
 
 %%Set matrice A
 %%Reads the text file, specifies the format, and sets the size
@@ -20,4 +40,17 @@ A = fscanf(fileID, formatSpec, sizeA);
 %%Red color
 plot(A(1,:), A(2 ,:),'r');
 
+fileID = fopen('shape2.txt','r'); 
 
+formatSpec = '%f %f';
+
+sizeB = [2 11];
+
+B = fscanf(fileID, formatSpec, sizeB);
+
+hold on
+plot(B(1,:), B(2 ,:),'r');
+
+C = A' * B
+
+plot(C(1,:), C(2 ,:),'r');
