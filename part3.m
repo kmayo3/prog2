c33aac9432b115186b9abd73bbeba0b7004cdcd0
kmayo3 <katlyn.mayo@ymail.com>
2015-10-13 19:04:59 -0600

    Images on Doc, Finished commenting

--- part3.m
+++ part3.m
@@ -3,45 +3,49 @@ clc
 clear all
 clf
 
+%%set the file name
+filename = 'newpart3.txt';
+
+%%Read in the file
+A = dlmread(filename, ' ',0,2);
+
 %%fileID reads in the text file
-%%fileID = fopen('part3.txt','r');
+fileID = fopen('shape2.txt','r'); 
+
 %%This specifies the format of the file that we are reading in
 formatSpec = '%f %f';
 
 %%This sets the size of the matrice to be read in
-%%sizeA = [1 6];
-
-%%Reads the text file, specifies the format, and sets the size
-filename = 'newpart3.txt';
-M = dlmread(filename, ' ',0,2);
-
-%%amat = A(1, 3:6);
-%%Atrans = dlmread('part3.txt','\t',0,6);
-
-%%Plots A
-%%Red color
-%%plot(A(1,:), A(2 ,:),'r');
-
-fileID = fopen('shape2.txt','r'); 
-
-formatSpec = '%f %f';
-
 sizeB = [2 11];
 
+%%Reads the text file, specifies the format, and sets the size
 B = fscanf(fileID, formatSpec, sizeB);
 
+%%Allows us to plot more than one item on graph
 hold on
 
+%%Plots B on the graph
+%%Red color
 plot(B(1,:), B(2,:),'r');
 
-C =  M' * B;
+%%C Matrix
+%%Transpose of A times B
+C =  A' * B;
 
+%%Plots C
+%%Purple color
 plot(C(1,:), C(2,:),'m');
 
+%%Set the file name
 filenameD = 'part3.txt';
 
+%%Read in the file
 D = dlmread(filenameD,' ',0,2);
 
+%%E Matrix
+%%Transpose of D times B
 E = D' * B;
 
+%%Plots E
+%%Blue color
 plot(E(1,:), E(2,:), 'b');
